43aa121ac723b1064dbd422142e8a450dd20a19c
cbkmutlu <cbkmutlu@gmail.com>
2025-05-07 04:45:06 +0300

    update www

--- index.php
+++ index.php
@@ -6,35 +6,22 @@ use System\Container\Container;
 use System\Starter\Starter;
 use System\Router\Router;
 
-// Autoload
-require_once __DIR__ . '/App/Config/Constants.php';
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Helpers
-$dir = __DIR__ . '/System/Helpers';
-foreach (scandir($dir) as $file) {
-   if (is_file($dir . '/' . $file) && !str_starts_with($file, '_') && str_ends_with($file, '.php')) {
-      require_once $dir . '/' . $file;
-   }
-}
-
-// Error Reporting
-if (ENV === 'production') {
-   error_reporting(0);
-   ini_set('display_errors', 0);
-   ini_set('display_startup_errors', 0);
-} else {
-   error_reporting(E_ALL);
-   ini_set('display_errors', 1);
-   ini_set('display_startup_errors', 1);
+// Requires
+require_once __DIR__ . "/App/Config/Constants.php";
+require_once __DIR__ . "/vendor/autoload.php";
+foreach (glob(__DIR__ . "/System/Helpers/*.php") as $filename) {
+   require_once $filename;
 }
 
 // Container
 $container = new Container();
 $container->register();
 
+// Exception
+set_exception_handler([$container->get('error'), 'handleException']);
+set_error_handler([$container->get('error'), 'handleError']);
+
 // Starter
 $router = new Router($container);
 $app = new Starter($router);
-$app->env('.env.local');
-$app->routes(['Config/Routes']);
+$app->run();
