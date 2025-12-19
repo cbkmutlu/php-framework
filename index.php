82d0e2f339f6fce996242f7842dcae6ecbed71d3
cbkmutlu <cbkmutlu@gmail.com>
2025-12-19 03:26:59 +0300

    update index

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Config\Swagger;
 use System\Router\Router;
 use System\Container\Container;
 
 // Requires
-require_once __DIR__ . "/App/Config/Constants.php";
-require_once __DIR__ . "/vendor/autoload.php";
-foreach (glob(__DIR__ . "/System/Helpers/*.php") as $filename) {
+require_once __DIR__ . '/App/Config/Constants.php';
+require_once __DIR__ . '/vendor/autoload.php';
+foreach (glob(__DIR__ . '/System/Helpers/*.php') as $filename) {
    require_once $filename;
 }
 
@@ -17,20 +16,16 @@ foreach (glob(__DIR__ . "/System/Helpers/*.php") as $filename) {
 $container = new Container();
 $container->register();
 
-// Exception
+// Exception Handler
 set_exception_handler([$container->get('error'), 'handleException']);
 set_error_handler([$container->get('error'), 'handleError']);
 
-// App Config
+// Import Env
 $config = import_config('defines.app');
 import_env($config['env']);
 
 // Router
 $router = new Router($container);
-
-// Swagger
-$swagger = new Swagger($router);
-$swagger->run();
 
 // Routes
 foreach (glob(ROOT_DIR . '/' .  $config['routes'] . '/*.php') as $route) {
