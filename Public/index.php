c28f380ba878ecf435d1ce82b4b99874aecf4dc0
cbkmutlu <cbkmutlu@gmail.com>
2025-12-31 15:47:04 +0300

    feat(web): initialize public entry point
    - Setup .htaccess for URL rewriting
    - Create index.php for app bootstrap
    - Configure routing, container, error handling

--- Public/index.php
+++ Public/index.php
@@ -2,13 +2,18 @@
 
 declare(strict_types=1);
 
+$parse_url = trim(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+if (php_sapi_name() === 'cli-server' && is_file($parse_url)) {
+   return false;
+}
+
 use System\Router\Router;
 use System\Container\Container;
 
 // Requires
-require_once __DIR__ . '/App/Config/Constants.php';
-require_once __DIR__ . '/vendor/autoload.php';
-foreach (glob(__DIR__ . '/System/Helpers/*.php') as $filename) {
+require_once dirname(__DIR__) . '/App/Config/Constants.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+foreach (glob(dirname(__DIR__) . '/System/Helpers/*.php') as $filename) {
    require_once $filename;
 }
 
